081ab9da48a83b5df215734620f5ea392ef558be
Jason <jvincentdsouza@gmail.com>
2021-06-20 13:53:32 +1000

    Forward ports to be accessible outside image container

--- dependencies/up.sh
+++ dependencies/up.sh
@@ -3,6 +3,7 @@
 docker pull neo4j
 
 docker run --detach --name=marcord-neo4j --rm --env=NEO4J_AUTH=none \
+--publish=7474:7474 --publish=7473:7473 --publish=7687:7687 \
 --volume=$HOME/neo4j/data:/data \
 --volume=$HOME/neo4j/import:/import \
 --volume=$HOME/neo4j/conf:/conf \
