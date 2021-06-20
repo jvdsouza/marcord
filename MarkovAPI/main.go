4ccbd62e911f55fdcf8ad730bc53312bb5e43983
Jason <jvincentdsouza@gmail.com>
2021-06-20 13:40:20 +1000

    Add predicates for testing the string prefix

--- MarkovAPI/main.go
+++ MarkovAPI/main.go
@@ -1,7 +1,15 @@
 package markovapi
 
+import "strings"
+
 type MarkovAPI struct{}
 
+var words []string = make([]string, 0)
+
 func (m MarkovAPI) ProcessTextMessage(s string) string {
-	return s
+	if strings.HasPrefix(s, "!read") || strings.HasPrefix(s, "!r") {
+		return strings.Join(words, ", ")
+	}
+	words = append(words, s)
+	return ""
 }
