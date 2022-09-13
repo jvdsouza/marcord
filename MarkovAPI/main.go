8f88c7e29f103abd179b2213b1cf7031772f0a67
Jason <jvincentdsouza@gmail.com>
2022-09-13 22:12:23 +1000

    Add some dummy return messages

--- MarkovAPI/main.go
+++ MarkovAPI/main.go
@@ -3,5 +3,12 @@ package markovapi
 type MarkovAPI struct{}
 
 func (m MarkovAPI) ProcessTextMessage(s string) string {
+	if s == "learn" {
+		return "entering learning mode"
+	}
+	if s == "talk" {
+		return "entering talking mode"
+	}
+
 	return s
 }
